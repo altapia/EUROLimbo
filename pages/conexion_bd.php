ef147989c3f7da2d52aa34bad58c5d47041f005f
altapia <altapia@gmail.com>
2012-06-15 12:14:25 +0200

    se elimina conexion_bd

--- pages/conexion_bd.php
+++
@@ -1,8 +0,0 @@
-<?
-date_default_timezone_set('Europe/Madrid');
-//Conexion con la base
-mysql_connect("localhost:3306","tapia","1234"); 
-
-//selecciÃ³n de la base de datos con la que vamos a trabajar 
-mysql_select_db("EURO2012"); 
-?>
